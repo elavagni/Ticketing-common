82c62ee7bb570170e9b39a46a9461613cecebacd
elavagni <elavagni@gmail.com>
2020-10-23 22:53:50 -0500

    Refactor publishing logic

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedEventPublisher } from './events/ticket-created-publisher';
+
 
 console.clear();
 
@@ -7,16 +9,18 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify ({
-        id:'123',
-        tite: 'Concert',
-        price: 20
-    });
-
-    stan.publish('ticket:created', data,  () => {
-        console.log('Event published');
-    });
+    try {
+        const publisher = new TicketCreatedEventPublisher(stan);
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20,
+            userId:'3254354'
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
